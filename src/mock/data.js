a36269aa06a6d9799d8960011f9620e83da61270
wtwerner <wtwerner@gmail.com>
2021-04-01 12:42:29 -0500

    Add about data

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,8 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpeg',
-  paragraphOne: '',
-  paragraphTwo: '',
+  paragraphOne:
+    'Recent bootcamp graduate with a background in Operations, Management, and Sales. Strong skills in team-building and project management, helps startups or established companies drive positive customer results and operational efficiencies.',
+  paragraphTwo: 'Experience with React, Rails, Node, and JavaScript.',
   paragraphThree: '',
   resume:
     'https://docs.google.com/document/d/e/2PACX-1vTJ-ahNenqb9TSLe-55U9E2HXRXkxRhxC1iPle1u7-65uj8a-oMdaAT_wL9ZHM3JPps7wSRQca9s20G/pub', // if no resume, the button will not show up
